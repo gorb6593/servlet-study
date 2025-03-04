ee4f3a0569d98c1a069848825829fe79609f8c6c
harry <gorb6593@gmail.com>
2025-03-05 01:13:05 +0900

    TIL - 서블릿

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,30 @@
+package hello.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member saveMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(saveMember.getId());
+        assertEquals(saveMember, findMember);
+
+    }
+}
